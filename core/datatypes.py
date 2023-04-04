18ceb49b6f49ce820ca5800b7220ce70c101cddc
petraxeman <pashamanprofirus@gmail.com>
2023-04-04 17:43:11 +0300

    Dev 3

--- core/datatypes.py
+++ core/datatypes.py
@@ -1,22 +1,75 @@
 from dataclasses import dataclass
-
+import os, yaml
 
 
 class Settings:
     def __init__(self, data: dict) -> None:
+        self.folder_name = None
         self.name = data['name']
+        self.keys = data['keys']
         self.start_record = data['replays']['start-record']
         self.fact_end_record = data['replays']['end-record']
         self.end_record = self.fact_end_record if self.fact_end_record != "None" else self.start_record
+        
+        self.resolution_type = data['replays']['resolution']['type']
+        self.text_resolution = data['replays']['resolution']['fact']
+        self.resolution = self.build_resolution(self.text_resolution) if self.resolution_type == 'custom' \
+                                                                    else self.build_resolution(self.resolution_type)
+
+        self.filtering = data['replays']['formating']['filtering']
+        self.threshold = data['replays']['formating']['opencv_threshold']
+        self.blur = data['replays']['formating']['opencv_blur']
+        self.threshold = int(self.threshold) if self.threshold != 'None' else None
+        self.blur = int(self.blur) if self.blur != 'None' else None
+
+    def build_resolution(self, string: str) -> tuple:
+        print(string)
+        x, y = string.split('x')
+        return (int(x), int(y))
+    
+    def build_yaml(self, file) -> None:
+        data = {'name' : self.name,
+                'keys' : self.keys,
+                'replays': {
+                    'start-record' : self.start_record,
+                    'end-record' : self.fact_end_record,
+                    'resolution' : {
+                        'type' : self.resolution_type,
+                        'fact' : self.text_resolution,
+                    },
+                    'formating' : {
+                        'filtering' : self.filtering,
+                        'opencv_threshold' : self.threshold,
+                        'opencv_blur' : self.blur
+                    }
+                }}
+        yaml.safe_dump(data, file)
 
     def __repr__(self) -> str:
         return f'<Setting for "{self.name}" {self.end_record}>'
+    
+    @classmethod
+    def create_empty(cls):
+        data = {'name' : 'Undefined',
+                'keys' : 'w,a,s,d',
+                'replays': {
+                    'start-record' : 'l',
+                    'end-record' : 'None',
+                    'resolution' : {
+                        'type' : '16x9',
+                        'fact' : '16x9',},
+                    'formating' : {
+                        'filtering' : 'True',
+                        'opencv_threshold' : '120',
+                        'opencv_blur' : '7'}}}
+        return cls(data)
+
 
 
 class GamesList:
     def __init__(self) -> None:
         self.dict = {}
-        self.current = None#self.dict[list(self.dict.keys())[0]]
+        self.current = None
 
     def set_current(self, short_name: str = None, full_name: str = None) -> None:
         if short_name:
@@ -26,6 +79,8 @@ class GamesList:
                 if self.dict[sn].name == full_name:
                     self.current = self.dict[sn]
                     break
+        else:
+            self.current = None
     
     def get_short_name(self, full_name: str) -> str:
         for short_name in self.dict:
@@ -34,8 +89,22 @@ class GamesList:
         raise Exception(f'Not found {repr(full_name)}')
 
     def add_game(self, short_name: str, settings: 'Settings') -> None:
+        settings.folder_name = short_name
         self.dict[short_name] = settings
     
+    def update_notes(self) -> None:
+        for element in os.listdir('.'):
+            if os.path.isdir(element) and element != 'TemplateGame':
+                if 'game-settings.yaml' in os.listdir(f'./{element}/'):
+                    game_settings = Settings(yaml.safe_load(open(f'./{element}/game-settings.yaml', 'r', encoding='utf8')))
+                    self.add_game(element, game_settings)
+        
+        exist_dirs = os.listdir('./')
+        for key in list(self.dict.keys()):
+            if key not in exist_dirs:
+                del self.dict[key]
+            
+
     def get_summary(self) -> list[tuple]:
         summary = []
         index = 0
--- core/ui.py
+++ core/ui.py
@@ -1,11 +1,12 @@
 # Импорты
 from asciimatics.screen import ManagedScreen, Screen
 from asciimatics.scene import Scene
-from asciimatics.widgets import Frame, ListBox, Layout, Divider, Text, Button, TextBox, Widget
+from asciimatics.widgets import Frame, ListBox, Layout, Divider, Text, Button, \
+    RadioButtons, TextBox, Widget, DropdownList, Label
 from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
 from time import sleep
-from core import utils
-import sys
+from core import utils, datatypes
+import sys, os
 
 # Глобальные переменные
 #
@@ -80,7 +81,7 @@ class GameManager(Frame):
         self.fix()
     
     def _reload_list(self, new_value = None):
-        self.games_list = utils.init_games()
+        self.games_list.update_notes()
         self.list_view.options = self.games_list.get_summary()
         self.list_view.value = new_value
     
@@ -93,7 +94,7 @@ class GameManager(Frame):
         raise NextScene('GameView')
 
     def _create_new(self,):
-        self.games_list.set_current(None)
+        self.games_list.current = datatypes.Settings.create_empty()
         raise NextScene("GameView")
     
     def _clone(self,):
@@ -115,27 +116,53 @@ class GameView(Frame):
                          title='Game View')
 
         self.games_list = games_list
+        self.is_new = False
         
         layout_1l = Layout([100])
         layout_2l = Layout([1, 1])
-        layout_3l = Layout([1, 1, 1, 1])
+        layout_3l = Layout([100])
+        layout_el = Layout([1, 1, 1, 1], fill_frame=True)
+
+        self.folder_name = Text("Folder name:", "folder_name")
         self.name_field = Text('Name:', 'name')
-        self.start_record_button = Text('Start record:', 'startrecordkey', max_length=4)
-        self.end_record_button = Text('End record:', 'endrecordkey', max_length=4)
+        self.keys_listening = Text('Keys:', 'keys')
         
+        self.start_record = Text('Start record:', 'startrecordkey', max_length=4)
+        self.end_record = Text('End record:', 'endrecordkey', max_length=4)
+
+        self.resolution_type = DropdownList([('16x9', 0), ('4x3', 1), ('custom', 2)], 'Resolution preset:', on_change=self._resolution_changed)
+        self.resolution_fact = Text('Resolution:', 'resolution')
+        self.filtering = RadioButtons([('On', 0), ('Off', 1)], 'Filtering', on_change=self._filtering_change)
+        self.threshold = Text('Threshold:', 'threshold', max_length=4)
+        self.blur = Text('Blur:', 'blur', max_length=4)
+
+        self.delete_button = Button('Delete', self._delete)
+
         self.add_layout(layout_1l)
         self.add_layout(layout_2l)
         self.add_layout(layout_3l)
+        self.add_layout(layout_el)
 
+        layout_1l.add_widget(Label('Main settings', height=2))
+        layout_1l.add_widget(self.folder_name)
         layout_1l.add_widget(self.name_field)
+        layout_1l.add_widget(self.keys_listening)
 
-        layout_2l.add_widget(self.start_record_button, 0)
-        layout_2l.add_widget(self.end_record_button, 1)
+        layout_2l.add_widget(self.start_record, 0)
+        layout_2l.add_widget(self.end_record, 1)
         layout_2l.add_widget(Divider())
 
-        layout_3l.add_widget(Button('Delete', self._delete), 0)
-        layout_3l.add_widget(Button('Save', self._save), 2)
-        layout_3l.add_widget(Button('Back', self._back), 3)
+        layout_3l.add_widget(Label('Replay settings', height=2))
+        layout_3l.add_widget(self.resolution_type)
+        layout_3l.add_widget(self.resolution_fact)
+        layout_3l.add_widget(self.filtering)
+        layout_3l.add_widget(self.threshold)
+        layout_3l.add_widget(self.blur)
+        layout_3l.add_widget(Divider())
+
+        layout_el.add_widget(self.delete_button, 0)
+        layout_el.add_widget(Button('Save', self._save), 2)
+        layout_el.add_widget(Button('Back', self._back), 3)
         
         self.fix()
     
@@ -144,18 +171,80 @@ class GameView(Frame):
     
     def _on_load(self,) -> None:
         if self.games_list.current is None:
+            self.delete_button.disabled = True
+            self.folder_name.disabled = False
+            self.is_new = True
             return
+        elif self.games_list.current.folder_name is None:
+            self.delete_button.disabled = True
+            self.folder_name.disabled = False
+            self.is_new = True
+        else:
+            self.delete_button.disabled = False
+            self.folder_name.disabled = True
+            self.is_new = False
+        
         self.current = self.games_list.current
         self.title = f'{self.current.name} view'
+        
+        self.folder_name.value = self.current.folder_name
         self.name_field.value = self.current.name
-        self.start_record_button.value = self.current.start_record
-        self.end_record_button.value = self.current.fact_end_record
-    
+        self.keys_listening.value = self.current.keys
+
+        self.start_record.value = self.current.start_record
+        self.end_record.value = self.current.fact_end_record
+
+        resdict = {'16x9': 0, '4x3': 1, 'custom': 2}
+        self.resolution_type.value = resdict[self.current.resolution_type]
+        self.resolution_fact.value = self.current.text_resolution
+
+        self.filtering.value = (int(bool(self.current.filtering)) - 1) * -1
+        self.threshold.value = str(self.current.threshold)
+        self.blur.value = str(self.current.blur)
+
+    def _resolution_changed(self) -> None:
+        value = self.resolution_type.value
+        if self.resolution_type.options[value][0] != 'custom':
+            self.resolution_fact.value = self.resolution_type.options[value][0]
+            self.resolution_fact.disabled = True
+        else:
+            self.resolution_fact.disabled = False
+
+    def _filtering_change(self) -> None:
+        if self.filtering.value == 1:
+            self.threshold.disabled = True
+            self.blur.disabled = True
+        else:
+            self.threshold.disabled = False
+            self.blur.disabled = False
+
     def _delete(self,) -> None:
         utils.delete(self.games_list.get_short_name(self.name_field.value))
         raise NextScene("GameManager")
     
     def _save(self,) -> None:
+        if self.folder_name.value in os.listdir('./') and self.is_new:
+            return
+        
+        if self.is_new:
+            os.mkdir(f'./{self.folder_name.value}')
+
+        self.current.name = self.name_field.value
+        self.current.keys = self.keys_listening.value
+        self.current.start_record = self.start_record.value
+        self.current.fact_end_record = self.end_record.value
+
+        self.current.resolution_type = self.resolution_type.options[self.resolution_type.value][0]
+        self.current.text_resolution = self.resolution_fact.value
+
+        a = self.filtering._options[self.filtering.value][1] - 1
+        self.current.filtering = bool(a)
+        self.current.threshold = self.threshold.value if self.threshold.value != '' else 'None'
+        self.current.blur = self.blur.value if self.blur.value != '' else 'None'
+
+        with open(f'./{self.folder_name.value}/game-settings.yaml', 'w', encoding='utf8') as file:
+            self.current.build_yaml(file)
+
         raise NextScene("GameManager")
 
     def _back(self,) -> None:
