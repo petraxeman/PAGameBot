07b87a07684ed5f300e05003e06936b59686ac92
petraxeman <pashamanprofirus@gmail.com>
2023-04-05 17:44:30 +0300

    Dev 3.5

--- record_server.py
+++ record_server.py
@@ -1,19 +1,35 @@
-import sys
 from core import utils
-from pynput.mouse import Listener as MListener
-from pynput.keyboard import Listener as KListener
+from pynput.mouse import Controller as MController
+from pynput.keyboard import Controller as KController
+from pynput import keyboard as Keyboard
+import sys, time, copy
 
 
-def button(key):
-    if key == 'q':
-        quit(0)
-        l.stop()
-    print(key)
+
+class KeyboardEvents():
+    def __init__(self) -> None:
+        self.list = []
+    def add_event(self, event):
+        self.list.append(event)
+    def get_events(self):
+        _ = copy.deepcopy(self.list)
+        self.list = []
+
+def keyboard_pressed(key):
+    try:
+        if key.char == 'q':
+#            klistener.stop()
+            quit(0)
+    except Exception:
+        pass
+    keyboard_events.add_event(key)
+
 
 
 if __name__ == '__main__':
     path, folder = sys.argv
     games = utils.init_games()
+    keyboard_events = KeyboardEvents()
 
     recording_game = games.dict[folder]
     
@@ -21,9 +37,18 @@ if __name__ == '__main__':
     key_to_end     = recording_game.end_record
     keys_to_listen = recording_game.keys.split(',')
 
-    l = KListener(button, button)
-    l.start()
-    l.join()
-    
-
+    frame_delay = 1/15
+"""    for i in range(10):
+        for z in [1, 2, 3, 4]:
+            with Keyboard.Events() as events:
+                print(f'I: {i}, Z: {z}')
+                
+                print(list(events))
+                #if events is not None:
+                    #print([event for event in events])
+                time.sleep(frame_delay)
+                continue
+"""
+    mouse = MController()
+    keyboard = KController()
     print(keys_to_listen)
